a413f871b953f2813cb40f30645484ddd0572c7c
Nhung <btcnhung.1299@gmail.com>
2019-04-08 20:15:59 +0700

    Updated btnConnect

--- ClientChat/ClientChat/CServerSettings.cpp
+++ ClientChat/ClientChat/CServerSettings.cpp
@@ -2,13 +2,14 @@
 #include "ClientChat.h"
 #include "CServerSettings.h"
 #include "afxdialogex.h"
-
+#include <afxsock.h>
 
 // CServerSettings dialog
 
 IMPLEMENT_DYNAMIC(CServerSettings, CDialog)
 
-CServerSettings::CServerSettings(CWnd* pParent /*=nullptr*/) : CDialog(IDD_ServerSettings, pParent) {
+CServerSettings::CServerSettings(CWnd* pParent /*=nullptr*/) : CDialog(IDD_ServerSettings, pParent), serverPort(0)
+{
 }
 
 CServerSettings::~CServerSettings() {
@@ -17,6 +18,7 @@ CServerSettings::~CServerSettings() {
 void CServerSettings::DoDataExchange(CDataExchange* pDX) {
 	CDialog::DoDataExchange(pDX);
 	DDX_IPAddress(pDX, inpServerIP, serverIP);
+	DDX_Text(pDX, inpServerPort, serverPort);
 }
 
 
@@ -30,7 +32,16 @@ END_MESSAGE_MAP()
 
 // CServerSettings message handlers
 void CServerSettings::OnBtnClickConnect() {
+	CSocket clnt;
+	clnt.Create();
 
+	CString cstrServerIP;
+	cstrServerIP.Format(L"%d", serverIP);
+
+	BOOL connectStatus = clnt.Connect(cstrServerIP, serverPort);
+	if (!connectStatus) {
+		AfxMessageBox(L"Can't connect");
+	}
 }
 
 void CServerSettings::OnBtnClickCancel() {
